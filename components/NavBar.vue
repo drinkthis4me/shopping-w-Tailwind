bb36a9663f9b99cf6a5be5e8c715cab3f4fc5163
james <jamesoffice.417@gmail.com>
2022-12-28 16:01:58 +0800

    uninstall Nuxt Icon module

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -28,14 +28,22 @@
             <button
               class="rounded border-2 border-transparent p-1 text-3xl text-gray-400 hover:bg-gray-700 hover:text-white focus:text-white focus:outline-none"
               aria-label="Search">
-              <Icon name="ic:sharp-search" />
+              <svg width="32" height="32" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14z" />
+              </svg>
             </button>
 
             <NuxtLink to="/cart">
               <button
                 class="rounded border-2 border-transparent p-1 text-3xl text-gray-400 hover:bg-gray-700 hover:text-white focus:text-white focus:outline-none"
                 aria-label="Shopping cart">
-                <Icon name="ic:baseline-shopping-cart" />
+                <svg width="32" height="32" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2zM1 2v2h2l3.6 7.59l-1.35 2.45c-.16.28-.25.61-.25.96c0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12l.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0 0 20 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2z" />
+                </svg>
               </button>
             </NuxtLink>
             <!-- Profile dropdown -->
@@ -47,7 +55,11 @@
                   id="user-menu"
                   aria-label="User menu"
                   aria-haspopup="true">
-                  <Icon name="ic:baseline-person" />
+                  <svg width="32" height="32" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4s-4 1.79-4 4s1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+                  </svg>
                 </button>
               </div>
               <transition
