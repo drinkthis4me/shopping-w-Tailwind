2be6ddf2f84dd6b842a592a854c98b1c81d7e23e
james <jamesoffice.417@gmail.com>
2022-12-29 17:27:50 +0800

    add lorem ipsum

--- pages/pages/about.vue
+++ pages/pages/about.vue
@@ -1,39 +1,12 @@
 <template>
   <div>
-    <h1>about</h1>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis,
-      facilis.
-    </p>
-    <p>
-      Itaque totam dignissimos deleniti architecto hic magnam harum voluptate
-      animi!
-    </p>
-    <p>
-      Odio quam praesentium reprehenderit sapiente placeat sunt voluptatem sed
-      inventore.
-    </p>
-    <p>
-      Illum asperiores doloribus eius expedita natus tempora praesentium quod
-      ex.
-    </p>
-    <p>Dolorem quas quam eaque eos perferendis magnam sunt quod deserunt!</p>
-    <p>
-      Eligendi itaque ad ab explicabo delectus accusamus, accusantium eius
-      architecto.
-    </p>
-    <p>
-      Numquam modi fugiat, similique iusto illum deserunt ratione nihil
-      necessitatibus.
-    </p>
-    <p>Consectetur voluptatum earum porro, minus sit cum optio quis odio?</p>
-    <p>
-      Repudiandae ex, delectus earum recusandae odio sit reprehenderit
-      voluptatum. Dolore!
-    </p>
-    <p>
-      Sint voluptas voluptates atque dolore iure delectus, officiis velit
-      aperiam.
-    </p>
+    <main class="m-10">
+      <div class="text-2xl mb-3">About</div>
+      <p v-for="n in 10" :key="n" class="text-base mb-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nihil
+        numquam laboriosam? Est enim sunt neque, reprehenderit aut esse minima
+        quo saepe, fugiat numquam, vel dignissimos illo alias? Eum, placeat.
+      </p>
+    </main>
   </div>
 </template>
--- pages/pages/privacy-policy.vue
+++ pages/pages/privacy-policy.vue
@@ -1,3 +1,10 @@
 <template>
-  <div><h1>privacy policy</h1></div>
+  <div><main class="m-10">
+      <div class="text-2xl mb-3">Privacy Policy</div>
+      <p v-for="n in 10" :key="n" class="text-base mb-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nihil
+        numquam laboriosam? Est enim sunt neque, reprehenderit aut esse minima
+        quo saepe, fugiat numquam, vel dignissimos illo alias? Eum, placeat.
+      </p>
+    </main></div>
 </template>
