0d4fb1f2b1a05814553335d57aa70107f7d96ca8
james <jamesoffice.417@gmail.com>
2023-01-16 15:25:44 +0800

    fix: typo

--- pages/products/bottoms/index.vue
+++ pages/products/bottoms/index.vue
@@ -21,9 +21,9 @@ const productsForBottoms = [
   {
     id: 1,
     name: 'Loose fit jeans',
-    href: '/products/tops/loose-fit-jeans',
+    href: '#',
     price: 1790,
-    description: 'The Basic Tee 6-Pack',
+    description: '100% cotten jeans fit all bodies',
     coverImage: {
       src: 'https://images.unsplash.com/photo-1582552938357-32b906df40cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
       alt: 'Model in jeans in front of stairs.',
@@ -31,35 +31,35 @@ const productsForBottoms = [
   },
   {
     id: 2,
-    name: 'Tshirt #2',
+    name: 'Bottoms #2',
     href: '#',
     price: 249,
     description: 'Lorem ipsum dolor sit amet consectetur',
     coverImage: {
       src: 'https://images.unsplash.com/photo-1591195853828-11db59a44f6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      alt: 'Placeholder image for jeas.',
+      alt: 'Placeholder image for jeans.',
     },
   },
   {
     id: 3,
-    name: 'Tshirt #3',
+    name: 'Bottoms #3',
     href: '#',
     price: 249,
     description: 'Lorem ipsum dolor sit amet consectetur',
     coverImage: {
       src: 'https://images.unsplash.com/photo-1591195853828-11db59a44f6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      alt: 'Placeholder image for jeas.',
+      alt: 'Placeholder image for jeans.',
     },
   },
   {
     id: 4,
-    name: 'Tshirt #4',
+    name: 'Bottoms #4',
     href: '#',
     price: 249,
     description: 'Lorem ipsum dolor sit amet consectetur',
     coverImage: {
       src: 'https://images.unsplash.com/photo-1591195853828-11db59a44f6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      alt: 'Placeholder image for jeas.',
+      alt: 'Placeholder image for jeans.',
     },
   },
 ]
