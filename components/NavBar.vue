9d0e2cf4bb64d9d33a23daaa132f07d251c28311
james <jamesoffice.417@gmail.com>
2023-02-01 17:55:20 +0800

    add hover info for cart icon

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -47,21 +47,32 @@
               </button>
             </NuxtLink>
             <NuxtLink to="/cart" v-else>
-              <div class="indicator">
-                <div class="indicator-item badge badge-secondary">
-                  {{ cartStore.numberOfItems }}
+              <div class="dropdown dropdown-end dropdown-hover">
+                <div class="indicator">
+                  <div class="indicator-item badge badge-secondary">
+                    {{ cartStore.numberOfItems }}
+                  </div>
+                  <button
+                    class="hover:bg-primary rounded border-2 border-transparent p-1 text-3xl text-gray-400 hover:text-slate-700 focus:text-white focus:outline-none"
+                    aria-label="Shopping cart">
+                    <svg width="32" height="32" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2zM1 2v2h2l3.6 7.59l-1.35 2.45c-.16.28-.25.61-.25.96c0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12l.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0 0 20 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2z" />
+                    </svg>
+                  </button>
                 </div>
-                <button
-                  class="hover:bg-primary rounded border-2 border-transparent p-1 text-3xl text-gray-400 hover:text-slate-700 focus:text-white focus:outline-none"
-                  aria-label="Shopping cart">
-                  <svg width="32" height="32" viewBox="0 0 24 24">
-                    <path
-                      fill="currentColor"
-                      d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2zM1 2v2h2l3.6 7.59l-1.35 2.45c-.16.28-.25.61-.25.96c0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12l.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0 0 20 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2z" />
-                  </svg>
-                </button>
+                <div
+                  tabindex="0"
+                  class="dropdown-content rounded bg-base-100 mt-3 w-52 shadow ring-2 ring-black ring-opacity-5">
+                  <div class="card-body">
+                    <span class="text-lg font-bold">{{ cartStore.numberOfItems }} Items</span>
+                    <span class="text-info">Subtotal: ${{ cartStore.subtotal.toLocaleString() }}</span>
+                  </div>
+                </div>
               </div>
             </NuxtLink>
+
             <!-- Profile dropdown if isLogin-->
             <div v-if="authStore.currentUser" class="relative ml-3">
               <div>
